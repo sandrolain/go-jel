d97d46764c07ba068e62fa57b5d3cfb6657e58b2
Sandro Lain <xande05@gmail.com>
2021-09-26 16:37:36 +0200

    Jobs Worker and example

--- example/main.go
+++ example/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
 
+	gojel "github.com/sandrolain/go-jel"
+)
+
+func main() {
+	// Start the pool of workers
+	jobs, results := gojel.StartJobs(func(value interface{}) interface{} {
+		fmt.Printf("Worker Input %v\n", value)
+		time.Sleep(time.Millisecond * 100)
+		return value.(int) * 2
+	}, 3)
+
+	go func() {
+		for res := range results {
+			fmt.Printf("Worker Result %v\n", res)
+		}
+	}()
+
+	counter := 0
+	itvStop, _ := gojel.SetInterval(func() {
+		counter++
+		fmt.Printf("\nInterval %v\n", counter)
+		jobs <- rand.Intn(100)
+	}, 250)
+	gojel.SetTimeout(func() {
+		itvStop <- true
+		fmt.Printf("Stop Interval!\n")
+	}, 3000)
+	toDone, _ := gojel.SetTimeout(func() {
+		fmt.Printf("End!\n")
+	}, 5000)
+	<-toDone
 }
--- gojel.go
+++ gojel.go
@@ -1,23 +1,62 @@
 package gojel
 
 import (
+	"fmt"
 	"time"
 )
 
-func SetTimeout(callback func(), millis int64) *time.Timer {
+func SetTimeout(callback func(), millis int64) (chan bool, *time.Timer) {
 	timer := time.NewTimer(time.Duration(millis * int64(time.Millisecond)))
+	done := make(chan bool)
 	go func() {
 		<-timer.C
 		callback()
+		done <- true
 	}()
-	return timer
+	return done, timer
 }
 
-func SetInterval(callback func(), millis int64) *time.Ticker {
+func SetInterval(callback func(), millis int64) (chan bool, *time.Ticker) {
 	ticker := time.NewTicker(time.Duration(millis * int64(time.Millisecond)))
+	done := make(chan bool)
 	go func() {
-		<-ticker.C
-		callback()
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				callback()
+			}
+		}
 	}()
-	return ticker
+	return done, ticker
+}
+
+func jobWorker(callback func(interface{}) interface{}, counter *int, id int, jobs <-chan interface{}, results chan<- JobResult) {
+	for j := range jobs {
+		*counter++
+		var jobNumber = *counter
+		fmt.Printf("Worker %v: started job %v with value: %v\n", id, jobNumber, j)
+		result := callback(j)
+		results <- JobResult{
+			WorkerNum: id,
+			Result:    result,
+		}
+		fmt.Printf("Worker %v: finished job %v\n", id, jobNumber)
+	}
+}
+
+type JobResult struct {
+	WorkerNum int
+	Result    interface{}
+}
+
+func StartJobs(callback func(interface{}) interface{}, workersNum int) (chan<- interface{}, <-chan JobResult) {
+	jobs := make(chan interface{}, workersNum)
+	results := make(chan JobResult, workersNum)
+	counter := 0
+	for w := 1; w <= workersNum; w++ {
+		go jobWorker(callback, &counter, w, jobs, results)
+	}
+	return jobs, results
 }
